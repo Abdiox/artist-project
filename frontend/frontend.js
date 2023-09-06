4411cee1a686789228af636de6a86974be7a288d
Abdiox <abdirahmanroble2610@gmail.com>
2023-09-06 12:04:25 +0200

    lavet det rigtigt!

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,52 +1,37 @@
 "use strict";
 
-// let artists;
-// const endpoint = "https://artist-api-32971-default-rtdb.europe-west1.firebasedatabase.app/";
-// window.addEventListener("load", start);
-
-// function start() {
-//   console.log("Velkommen!");
-//   getArtists();
-// }
-
-// async function getArtists() {
-//   const response = await fetch(`${endpoint}.json`);
-//   const data = await response.json();
-//   console.log(data);
-
-//   const artists = await artistData(data);
-//   displayArtists(artists);
-// }
-
 let artists;
+const endpoint = "http://localhost:5000";
 window.addEventListener("load", start);
 
-function start() {
+async function start() {
   console.log("Velkommen!");
-  getArtists();
+  await getArtists();
+  updateGrid();
+  console.log(artists);
 }
 
 async function getArtists() {
-  const response = await fetch(`/data/artists.json`);
+  const response = await fetch(`${endpoint}/artist`);
   const data = await response.json();
-  const artists = await artistData(data);
+  artists = data;
+  console.log(artists);
   displayArtists(artists);
 }
 
-async function artistData(dataObject) {
-  const artistArray = [];
-  for (const key in dataObject) {
-    const artist = dataObject[key];
-    artist.id = key;
-    artistArray.push(artist);
-  }
-  return artistArray;
+//-------------------Update Grid----------------------//
+
+async function updateGrid() {
+  await getArtists();
+  console.log(artists);
+  displayArtists(artists);
+  console.log(artists);
 }
 
-function displayArtists(listOfArtists) {
+function displayArtists(listOfArtist) {
   document.querySelector("#artists").innerHTML = "";
-  for (const artists of listOfArtists) {
-    showArtists(artists);
+  for (const artist of listOfArtist) {
+    showArtists(artist);
   }
 }
 
@@ -68,6 +53,7 @@ function showArtists(artistObject) {
   document.querySelector("#artists").insertAdjacentHTML("beforeend", html);
   document.querySelector("#artists article:last-child").addEventListener("click", () => artistClicked(artistObject));
 }
+
 //-------------------Show Dialog----------------------//
 function artistClicked(artistObject) {
   console.log("Detail view opened");
@@ -83,4 +69,57 @@ function showDialog(artistObject) {
   document.querySelector("#genres").textContent = artistObject.genres;
   document.querySelector("#activeSince").textContent = artistObject.activeSince; // Ændret til "activeSince"
   document.querySelector("#website").textContent = artistObject.website;
+}
+
+//-------------------Create Artist----------------------//
+
+function showCreateModal() {
+  const dialog = document.querySelector("#dialog-create-game");
+
+  dialog.showModal();
+
+  document.querySelector("#createGame").addEventListener("submit", createGameClicked);
+
+  dialog.querySelector(".close").addEventListener("click", () => {
+    dialog.close();
+  });
+}
+
+async function createArtistClicked(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const image = form.image.value;
+  const name = form.name.value;
+  const shortDescription = form.shortDescription.value;
+  const birthdate = form.birthdate.value;
+  const genres = form.genres.value;
+  const activeSince = form.activeSince.value;
+  const website = form.website.value;
+
+  const response = await createArtist(image, name, shortDescription, birthdate, genres, activeSince, website);
+
+  if (response.ok) {
+    form.reset();
+    updateGrid();
+  }
+  document.querySelector("#dialog-create-artist").close();
+}
+
+async function createArtist(image, name, shortDescription, birthdate, genres, activeSince, website) {
+  const newArtist = {
+    image: image,
+    name: name,
+    shortDescription: shortDescription,
+    birthdate: birthdate,
+    genres: genres,
+    activeSince: activeSince,
+    website: website,
+  };
+  const postAsJson = JSON.stringify(newArtist);
+
+  const response = await fetch(`/data/artists.json`, { method: "POST", body: postAsJson });
+
+  return response;
 }
