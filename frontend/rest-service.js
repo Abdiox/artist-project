34e44d25863e8b09031ca524f399687214522a18
Abdiox <abdirahmanroble2610@gmail.com>
2023-09-07 00:30:53 +0200

    DELETE VIRKER!

--- frontend/rest-service.js
+++ frontend/rest-service.js
@@ -37,9 +37,6 @@ async function createArtist(id, image, name, shortDescription, birthdate, genres
 async function deleteArtist(id) {
   const response = await fetch(`${endpoint}/artist/${id}`, {
     method: "DELETE",
-    // headers: {
-    //   "Content-Type": "application/json",
-    // },
   });
   return response;
 }
