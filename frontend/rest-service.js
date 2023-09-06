9f3bf33f569e7de2f911fefa81ffc9e796f69a0f
Abdiox <abdirahmanroble2610@gmail.com>
2023-09-07 00:50:52 +0200

    lige added function update i rest-service + html ting for update

--- frontend/rest-service.js
+++ frontend/rest-service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export { endpoint, getArtists, createArtist, deleteArtist };
+export { endpoint, getArtists, createArtist, deleteArtist, updateArtist };
 
 const endpoint = "http://localhost:5500";
 
@@ -9,6 +9,8 @@ async function getArtists() {
   const data = await response.json();
   return data;
 }
+
+//-------------------Create Artist----------------------//
 
 async function createArtist(id, image, name, shortDescription, birthdate, genres, activeSince, website) {
   const newArtist = {
@@ -33,6 +35,34 @@ async function createArtist(id, image, name, shortDescription, birthdate, genres
 
   return response;
 }
+
+//-------------------Update Artist----------------------//
+
+async function updateArtist(id, image, name, shortDescription, birthdate, genres, activeSince, website) {
+  const updatedArtist = {
+    id: id,
+    image: image,
+    name: name,
+    shortDescription: shortDescription,
+    birthdate: birthdate,
+    genres: genres,
+    activeSince: activeSince,
+    website: website,
+  };
+
+  const json = JSON.stringify(updatedArtist);
+  const response = await fetch(`${endpoint}/games/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: json,
+  });
+
+  return response;
+}
+
+//-------------------Delete Artist----------------------//
 
 async function deleteArtist(id) {
   const response = await fetch(`${endpoint}/artist/${id}`, {
